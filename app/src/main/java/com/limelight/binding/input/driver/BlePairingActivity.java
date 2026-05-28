7bfac407670c445808b8252c477c92e5be949eb1
rodrigmatrix <rodrigmatrix@gmail.com>
2026-05-28 01:10:24 -0300

    update

--- app/src/main/java/com/limelight/binding/input/driver/BlePairingActivity.java
+++ app/src/main/java/com/limelight/binding/input/driver/BlePairingActivity.java
@@ -11,6 +11,7 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
